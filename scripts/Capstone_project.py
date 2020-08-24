9ea30516858c2290ac79591a487c55e31a6cf307
Ray Medina <medray529@gmail.com>
2020-08-24 15:28:06 -0400

    IBM Data Science Capstone Course Project

---
+++ scripts/Capstone_project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM Data Science Capstone Course Project
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+print ("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
