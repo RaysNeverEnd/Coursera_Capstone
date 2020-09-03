e6156d14262b5450e9d23c0d11f87d7a777d8e57
Ray Medina <medray529@gmail.com>
2020-09-02 21:52:49 -0400

    Coursera Capstone Project

---
+++ scripts/Coursera_Capstone_Project.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction/Business Problem:
+
+# #### I will use the Collisions data provided by the Coursera Data Science Capstone Course to provide some insight into the weather and road conditions that led to head-on collisions in Seattle. This could help shed some light into these catastrophic collisions and bring some awareness towards the conditions surrounding these events.
+
+#  
+
+# # Data:
+
+# #### The data provided includes collisions in Seattle from 2004 to 2020. Using this, I will provide some analytics into the conditions surrounding head-on collisions. 
+
+#  
+
+# In[ ]:
+
+
+
+
