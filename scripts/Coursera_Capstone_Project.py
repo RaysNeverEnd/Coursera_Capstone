c9a63468242a0f7af956094f727fe861fa55aa70
Ray Medina <medray529@gmail.com>
2020-09-03 08:14:56 -0400

    Coursera Capstone Project

--- scripts/Coursera_Capstone_Project.py
+++ scripts/Coursera_Capstone_Project.py
@@ -3,13 +3,19 @@
 
 # # Introduction/Business Problem:
 
-# #### I will use the Collisions data provided by the Coursera Data Science Capstone Course to provide some insight into the weather and road conditions that led to head-on collisions in Seattle. This could help shed some light into these catastrophic collisions and bring some awareness towards the conditions surrounding these events.
+# #### Vehicle collisions are a serious issue throughout the country and various measures are taken each year to reduce collisions and improve road safety. Although this issue has great focus, there seems to be minimal impact on reducing the number and severity of collisions. We are looking to understand the key factors associated to head-on collisions and determine if there is a correlation and/or a pattern associated with them in order to find additional ways to minimize head-on collisions. This should benefit the drivers in the Seattle area.
+# #### I will use the Collisions data provided by the Coursera Data Science Capstone Course to provide some insight into the weather conditions, road conditions, severity of the collisions, whether the drivers were speeding or not and other data that led to head-on collisions in Seattle. The data was collected by the Seattle Police Department. 
 
 #  
 
 # # Data:
 
-# #### The data provided includes collisions in Seattle from 2004 to 2020. Using this, I will provide some analytics into the conditions surrounding head-on collisions. 
+# #### The data provided includes collisions in Seattle from 1/1/2004 to 5/20/2020 and it includes data from over 190,000 accidents. Included as part of that data were information regarding where the accident included personal injury or just physical damage in addition to road conditions, weather, location, severity of the collision, whether the drivers were speeding, etc.
+# #### With over 190,000 accidents there is enough data to train and test a model to facilitate results. The data includes 37 variables per accident which include key variables to determine the leading conditions for head-on collisions.
+# 
+# #### Data Source: The Coursera Capstone project provided a link to download the data for this project, the Data-Collisions.csv. 
+# 
+# #### https://s3.us.cloud-object-storage.appdomain.cloud/cf-courses-data/CognitiveClass/DP0701EN/version-2/Data-Collisions.csv
 
 #  
 
